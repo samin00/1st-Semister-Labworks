16b22cbbd53ba4c9b78f0185cbcbe0e6cad323cb
Prince BIlly Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2017-10-20 02:01:33 +0600

    Completed section 2

---
+++ 2.f.c
@@ -0,0 +1,55 @@
+//Write a program using an array of structure to read the number of three subjects for n
+//students and sort them according to their position. (1st, 2nd, 3rd ......)
+#include<stdio.h>
+
+struct student{
+    char name[94];
+    int roll;
+    float marks[3],tm;
+};
+
+int main(){
+    int n,i,j;
+    struct student s[4], temp;
+
+
+    for(i=0;i<n;i++){
+        s[i].tm=0;
+        printf("Student %d:\n",i+1);
+        printf("Name:\t");
+        scanf("%s",&s[i].name);
+        printf("Roll:\t");
+        scanf("%d",&s[i].roll);
+        for(j=0;j<3;j++){
+            printf("Subject %d:",j+1);
+            scanf("%f",&s[i].marks[j]);
+            s[i].tm += s[i].marks[j];
+
+        }
+
+    }
+    //sorting
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++)
+            if(s[j].tm<s[j+1].tm){
+                temp=s[j];
+                s[j]=s[j+1];
+                s[j+1]=temp;
+            }
+    }
+    //Output
+    for(i=0;i<n;i++){
+        printf("\n\nMerit %d:\n",i+1);
+        printf("Name:\t%s\n",s[i].name);
+        printf("Roll:\t%d\n",s[i].roll);
+        for(j=0;j<3;j++){
+            printf("Subject %d mark: %f\n",j+1,s[i].marks[j]);
+        }
+         printf("Total Mark:\t%f\n\n",s[i].tm);
+    }
+
+
+    return 0;
+}
+
+
